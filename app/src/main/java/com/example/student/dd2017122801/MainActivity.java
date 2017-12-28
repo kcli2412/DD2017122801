9c5b8f4842d01ee2480be5b04d406087c7f8adcc
kcli2412 <kcli2412@gmail.com>
2017-12-28 10:13:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/dd2017122801/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.student.dd2017122801;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View v)
+    {
+        TextView tv = findViewById(R.id.textView);
+        tv.setText("Good Morning");
+    }
+}
