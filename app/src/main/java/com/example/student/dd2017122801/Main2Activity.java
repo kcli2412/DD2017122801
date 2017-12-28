e69f03f98422b3bc6690fd0673e3fb958eebcc22
kcli2412 <kcli2412@gmail.com>
2017-12-28 12:13:37 +0800

    新增第三頁,將第三頁的資料傳回第一頁.

---
+++ app/src/main/java/com/example/student/dd2017122801/Main2Activity.java
@@ -0,0 +1,28 @@
+package com.example.student.dd2017122801;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void clickOkay(View v)
+    {
+        Intent it = new Intent();
+        it.putExtra("myresult","ABC123");
+        setResult(RESULT_OK, it);
+        finish();
+    }
+
+    public void clickCancel(View v)
+    {
+
+    }
+}
--- app/src/main/java/com/example/student/dd2017122801/MainActivity.java
+++ app/src/main/java/com/example/student/dd2017122801/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.student.dd2017122801;
 
+import android.app.Instrumentation;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,16 +9,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.textView);
     }
 
     public void click1(View v)
     {
-        TextView tv = findViewById(R.id.textView);
         tv.setText("Good Morning");
     }
 
@@ -27,5 +30,17 @@ public class MainActivity extends AppCompatActivity {
         EditText et = findViewById(R.id.editText);
         it.putExtra("data", et.getText().toString());
         startActivity(it);
+    }
+
+    public void click3(View v)
+    {
+        Intent it=new Intent(MainActivity.this, Main2Activity.class);
+        startActivityForResult(it,123);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        tv.setText(data.getStringExtra("myresult"));
     }
 }
